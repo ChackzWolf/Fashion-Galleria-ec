bfe9e000215c9a2d275cfeea423a4a8426b7e7b2
ChackWolf <jacksoncheriyan05@gmail.com>
2024-07-26 12:29:47 +0530

    added docker and kubernetes configs

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const saltRounds = 10;
 const UserModel = require("../models/User")
 const ProductModel = require("../models/Product")
